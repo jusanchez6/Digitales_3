5eca95d1535d851a92b06591a50de05cf42eb21c
juliansanchez6 <julian.sanchez6@udea.edu.co>
2024-10-28 14:38:37 -0500

    documentacion de la basura hecha en micropython

--- Lab4/MicroPython/main.py
+++ Lab4/MicroPython/main.py
@@ -1,3 +1,16 @@
+"""
+@file main.py
+@brief  Archivo principal del proyecto.
+
+Este archivo contiene el código principal del proyecto. Se encarga de leer la señal PWM de un pin de entrada y mostrar la frecuencia y el ciclo de trabajo en un display de 7 segmentos.
+
+@authors Maria Del Mar Arbelaez
+        Julian Mauricio Sanchez
+
+@date 2024-10-28
+"""
+
+
 from machine import Pin
 import time
 
@@ -30,6 +43,15 @@ lookup = [
 ]
 
 def pwm_callback(pin):
+    """
+    @brief Callback para la señal PWM.
+    
+    Esta función se llama cuando se detecta un flanco de subida o bajada en la señal PWM.
+
+    @param pin Pin de entrada.
+
+    @return None
+    """
     global last_rising_edge, period, frequency, duty_cycle, pulse_high_time
     
     current_time = time.ticks_us()  # Captura el tiempo actual en microsegundos
@@ -46,7 +68,16 @@ def pwm_callback(pin):
             duty_cycle = (pulse_high_time / period) * 100
 
 def init():
-    """Inicializa el display de 7 segmentos."""
+    """
+    @brief Inicializa el display de 7 segmentos.
+
+    Esta función inicializa el display de 7 segmentos apagando todos los segmentos y displays.
+
+    @param None
+
+    @return None
+
+    """
     for seg_pin in SEGMENT_PINS:
         seg_pin.off()
     for en_pin in enable_pins:
@@ -54,13 +85,29 @@ def init():
     print("Display de 7 segmentos inicializado.")
 
 def write_value(value):
-    """Escribe un valor en el display de 7 segmentos."""
+    """
+    @brief Escribe un valor en el display de 7 segmentos.
+    
+    Esta función escribe un valor en el display de 7 segmentos.
+
+    @param value Valor a escribir en el display.
+
+    @return None
+    """
     bits = lookup[value]
     for i, seg_pin in enumerate(SEGMENT_PINS):
         seg_pin.value((bits >> i) & 1)
 
 def write_decimals(value):
-    """Muestra el valor en el display de 7 segmentos."""
+    """
+    @brief Muestra el valor en el display de 7 segmentos.
+    
+    Esta función se encarga de gestionar la multiplexacion en un display de 7 segmentos triple
+
+    @param value Valor a mostrar en el display.
+
+    @return None
+    """
     for run in range(3):  # Muestra en 3 dígitos
         # Apaga el display anterior
         if run > 0:
@@ -90,6 +137,16 @@ def write_decimals(value):
 
 
 def main():
+    """
+    @brief Función principal del programa.
+
+    Esta función es la función principal del programa. Se encarga de inicializar el pin de entrada y el display de 7 segmentos, y de mostrar la frecuencia y el ciclo de trabajo de la señal PWM.
+
+    @param None
+
+    @return None
+
+    """
     pwm_pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=pwm_callback)
 
     # Inicializa el display de 7 segmentos
